850db6fba813029d0af6b8bb7a68f6b1d99eb064
karandeepsingh3030 <karandeep@nexgeniots.com>
2024-07-07 17:00:45 +0530

    connecting db

--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
-import { config } from 'dotenv'
-import express from 'express'
-import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from './db/index.js'
 
-var whitelist = ['http://localhost:3000']
-var corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-  }
+dotenv.config()
 
-
-let app = express()
-app.use(cors(corsOptions))
-// app.use(express.static('dist')) directly render the FE using middleware (not a good practice)
-config()
-
-let port = process.env.PORT
-
-// app.get('/api',(req,res) => {
-//     return res.send("server is ready")
-// })
-
-
-app.listen(port,() => {
-    console.log(`server connected successfully at ${port}`)
-} )
+connectDB()
