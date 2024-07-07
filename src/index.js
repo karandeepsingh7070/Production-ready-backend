83447abfb8e5c5b345df956f29ce5809089e5f20
karandeepsingh3030 <karandeep@nexgeniots.com>
2024-07-07 17:14:23 +0530

    creating server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import dotenv from "dotenv"
 import connectDB from './db/index.js'
+import { app } from "./app.js"
 
 dotenv.config()
 
 connectDB()
+.then(() => {
+    let port = process.env.PORT || 8080
+    app.listen(port, () => {
+        console.log(`Server initiated at ${port}`)
+    })
+})
+.catch((err) => {
+    app.on(('error', (err) => {
+        console.log(err)
+        process.exit(1)
+    }))
+    console.log("MongoDB connection failed")
+})
