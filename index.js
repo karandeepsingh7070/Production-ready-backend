f7f78c0be35933e3b056f19f605a9475b37d37c1
karandeepsingh3030 <karandeep@nexgeniots.com>
2024-07-07 11:40:28 +0530

    setup express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("production ready backend")
+let express = require('express')
+let app = express()
+
+let port = 8080
+
+app.get('/',(req,res) => {
+    return res.send("server connected")
+})
+app.get('/login',(req,res) => {
+    return res.send("<h1>Login</h1>")
+})
+
+
+app.listen(port,() => {
+    console.log(`server connected successfully ${port}`)
+} )
