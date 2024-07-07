e2de81d47de1665467ce4de3490ac54239502638
karandeepsingh3030 <karandeep@nexgeniots.com>
2024-07-07 19:19:15 +0530

    setting up structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
-import express from "express"
+import express, { urlencoded } from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 
 let app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(urlencoded({extended: true}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 export {app}
